624b8bedd93360dcab0b3e6c7ef682c54135ec85
alexanderpharwood <alex.harwood@live.co.uk>
2019-07-02 00:30:07 +0100

    Has now checks for root and / or value for nested properties

--- src/api/has.js
+++ src/api/has.js
@@ -1,11 +1,15 @@
 import getRoot from './getRoot.js';
+import getValue from './getValue.js';
 
 function has(path) {
 	if (typeof path !== 'string') {
 		throw new TypeError("path must be of type 'string': '" + typeof path + "' given.");
 	}
 	
-	return typeof getRoot(path) !== 'undefined';
+	let rootExists = typeof getRoot(path) !== 'undefined';
+	let valueExists = typeof getValue(path) !== 'undefined';
+	
+	return rootExists || valueExists;
 }
 
 export default has;
